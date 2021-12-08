3d20a668dd29536540d2ba4f21a28db13d5ec4d4
Amy <llamanozz@gmail.com>
2021-12-08 12:30:08 +0100

    Initial commit

--- day 7/script.js
+++ day 7/script.js
@@ -5,7 +5,6 @@ fetch(inputTxt)
   .then(res => res.text())
   .then(data => {
     let input = data.split(",").map(Number)
-
     console.log(bothParts(input, "part1")); // 356992
     console.log(bothParts(input, "part2"));  // 101268110
   })
@@ -21,10 +20,13 @@ const bothParts = (input, part) => {
     // tempFuel stores the current amount of fuel needed as the loops are running
     let tempFuel = 0;
     input.forEach((el, j) => {
+      // for part 1, the difference between the current value and the target value is linear (so 4 steps costs 1 + 1 + 1 + 1 = 4)
       if (part === "part1") tempFuel += Math.abs(input[j]-i);
+      // for part 2, each step costs 1 extra fuel (so 4 steps costs 1 + 2 + 3 + 4 = 10), so use triangle numbers, the formula is (n*(n+1))/2
       if (part === "part2") tempFuel += ((Math.abs(input[j]-i)*(Math.abs(input[j]-i)+1))/2);
     });
 
+    // initially set amountOfFuel with the first value, then overwrite it each time the current value is lower to get the lowest amount needed
     if (!amountOfFuel) {
       amountOfFuel = tempFuel;
     } else if (tempFuel < amountOfFuel) amountOfFuel = tempFuel;
